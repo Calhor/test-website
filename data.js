946d75e4dde89b6a634958e6baa735fb181c6bb4
Calhor <calhor@gmail.com>
2016-02-15 13:31:03 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,8 @@
+data = {
+
+new: { content: "News Check"+"\n"+"here for any news updates." ,title: "News"},
+con: { content: "Contact us"+"\n"+"Contact us at calhor@gmail.com"},
+who: { content: "Who we are"+"\n"+"There he is", title: "Who we are"},
+pf: { content: "Portfolio"+"\n"+"Here's some stuff we made", title: "Portfolio"}
+
+}
